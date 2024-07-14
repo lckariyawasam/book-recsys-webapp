a1571f718adda0056fa69d134895de99c4e91493
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-07-14 20:12:49 +0530

    Created Client Components for Sign In and Sign Up pages

---
+++ app/_components/SignIn.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { signIn } from 'next-auth/react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function SignInPageComponent() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    const result = await signIn('credentials', {
+      email,
+      password,
+      callbackUrl:'/test'
+    });
+
+    if (!result?.error) {
+      console.log('success', result);
+
+    } else {
+      console.log('error', result.error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
+    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
+      <h1 className="text-2xl font-bold text-center">Sign In</h1>
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Email
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md outline-none shadow-sm focus:ring-primary focus:border-primary sm:text-sm"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md outline-none shadow-sm focus:ring-primary focus:border-primary sm:text-sm"
+            />
+          </label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 text-sm font-medium text-white bg-primary border border-transparent rounded-md shadow-sm hover:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:priamry-500"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p className="text-sm text-center text-gray-600">
+        Don't have an account?{" "}
+        <Link
+          href="/auth/signup"
+          className="font-medium text-primary hover:text-primary-400"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  </div>
+  );
+}
