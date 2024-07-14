2e0de7fc60e15616500ace9abe0ba14c1f479859
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2024-07-14 17:42:34 +0530

    added Credentials Auth

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import CustomButton from '../_components/Button'
+import { auth } from '@/auth'
+import { SignOutButton } from '../_components/SignOutButton' 
 
-const Testing = () => {
+const Testing = async () => {
+
+
+  const session = await auth()
+
   return (
     <>
         <CustomButton variant="secondary" >Hello</CustomButton>
         <CustomButton variant="outline" >Hello</CustomButton>
-        <CustomButton size="large" >Hello</CustomButton>
+        <CustomButton size="large" minwidth='w-42' >Hello</CustomButton>
+        {session?.user ? <p>{session.user.email}</p> : <p>Logged out</p>}
+        <SignOutButton />
     </>
   )
 }
---
+++ utils/db.ts
@@ -0,0 +1,26 @@
+import prisma from '@/prisma/prisma_client'
+
+
+export async function getUserByEmail(email: string) {
+    return prisma.user.findUnique({ where: { email } });
+  }
+
+  export async function createUser({ email, password, name }: { email: string; password: string; name: string }) {
+    
+    console.log(email, password, name);
+    
+    try {
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password,
+          name,
+        },
+      });
+      console.log(user);
+      return user;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw error;
+    }
+  }
