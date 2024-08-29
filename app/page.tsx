03a2dadcf11197e185c476ba90777ba4dea55a8f
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-08-30 03:01:00 +0530

    Update API route and page content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Typography variant="h1" className="text-primary">Hello Nisansa</Typography>
+    <Typography variant="h1" className="text-primary">Hello World</Typography>
   );
 }
