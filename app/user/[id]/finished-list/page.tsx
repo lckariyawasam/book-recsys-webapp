b84d773129587a2d3e4750fbddb5a090e8ddbb45
Lakindu Kariyawasam <lckariyawasam@yahoo.com>
2024-10-18 20:16:05 +0530

    Add wishlist and finished list

--- app/user/[id]/finished-list/page.tsx
+++ app/user/[id]/finished-list/page.tsx
@@ -1,10 +1,70 @@
+"use client"
 import { BooksTable } from "@/app/_components/users/BooksTable/BooksTable"
+import { auth } from "@/auth";
+import prisma from "@/prisma/prisma_client"
+import { use } from "chai";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
+// const wishlist = await prisma.wishlist.findMany()
+
+interface BookListBook {
+  bookId: string,
+  title: string,
+  author: string,
+  genres: string,
+  imageURL?: string
+}
+
+const getBooks = async (userId: string) => {
+
+  const ratingsResponse = await fetch(`/api/rated/get`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "userId": userId }),
+  })
+
+  let ratedItems = await ratingsResponse.json()
+
+  let books:BookListBook[] = ratedItems.map((ratedItem:any) => {
+    return {
+      bookId: ratedItem.book.bookId,
+      title: ratedItem.book.title,
+      author: ratedItem.book.author,
+      genres: ratedItem.book.genres,
+      imageURL: ratedItem.book.imageURL
+    }
+  })
+
+  if (books.length === 0) {
+    return []
+  }
+
+  return books
+}
 
 const FinishedListPage = () => {
+  // Get the users id
+  const { id } = useParams() as { id: string }
+
+  const [books, setBooks] = useState<BookListBook[]>([])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const booksRetrieved = await getBooks(id as string)
+      setBooks(booksRetrieved)
+    }
+    fetchBooks()
+  }, [id])
+
+
   return (
     <div className="space-y-5">
         <div className="text-3xl font-medium text-gray-600">My Finished List</div>
-        <div className="border-l-2 border-t-2 p-3 rounded-md"><BooksTable /></div>
+        <div className="border-l-2 border-t-2 p-3 rounded-md"><BooksTable books={books} userId={id} /></div>
     </div>
   )
 }
